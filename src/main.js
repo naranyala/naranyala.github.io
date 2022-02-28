c56745a1cdaad01b5e49af152015a140c3f9e1d3
naranyala <fudzer.mh@gmail.com>
2022-02-28 08:49:30 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// const { createApp } = require('vue');
+// const App = './App.vue';
+
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const { resolve } = require('path')
+const { defineConfig } = require('vite');
+const vue = require('@vitejs/plugin-vue');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
+  }
 })
